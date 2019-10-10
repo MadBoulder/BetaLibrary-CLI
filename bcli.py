f5436c513d783090f028b107d272d4d72a586e9d
Juan Gallostra Acín <jgallostraa@jgingenieros.es>
2019-10-10 10:15:42 +0200

    Move constants to a new file

--- bcli.py
+++ bcli.py
@@ -6,25 +6,7 @@ import json
 import re
 import unicodedata
 
-CONFIG_FILE = "config.txt"
-# Actions
-CREATE = 0
-MODIFY = 1
-DELETE = 2
-EXIT = 3
-# Object sppecification
-ZONE = 0
-SECTOR = 1
-# Betalibrary's path specifications
-DATA = '/data'
-ZONES = '/zones'
-SECTORS = '/sectors'
-SEPARATOR = '/'
-# Special fields
-NUMERICS = ['parkings', 'sectors', 'guides']
-AUTOCOMPUTED = ['sector_data']
-TEMPLATED_ZONE_CREATION = ['name', 'parkings', 'sectors', 'guides']
-
+from constants import *
 
 def cli_configured(config_filename):
     """
---
+++ constants.py
@@ -0,0 +1,18 @@
+CONFIG_FILE = "config.txt"
+# Actions
+CREATE = 0
+MODIFY = 1
+DELETE = 2
+EXIT = 3
+# Object sppecification
+ZONE = 0
+SECTOR = 1
+# Betalibrary's path specifications
+DATA = '/data'
+ZONES = '/zones'
+SECTORS = '/sectors'
+SEPARATOR = '/'
+# Special fields
+NUMERICS = ['parkings', 'sectors', 'guides']
+AUTOCOMPUTED = ['sector_data']
+TEMPLATED_ZONE_CREATION = ['name', 'parkings', 'sectors', 'guides']
